9fb92148906fe2d0ee72bd36a336e2de7ce276f9
yuguang <yuguang@xiaomi.com>
2017-06-12 11:11:10 +0800

    1. delete useless prefix 2. add demo file, translate logcat.txt -> NMEASentence.txt Signed-off-by: yuguang <yuguang@xiaomi.com>

--- Nmea_filter.py
+++ Nmea_filter.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import sys
-import os
 
 '''
 author:yuguang
@@ -12,11 +11,13 @@ print 'your file is ', sys.argv[1], 'let\'s read & filter it.'
 sourceFile = sys.argv[1]
 targetFile = "NMEASentence.txt"
 compareObject=['$GPGSV', '$GLGSV', '$GAGSV','$GPGSA','$GNGSA','$GPVTG','$GPRMC','$GPGGA','$GNVTG', '$GNRMC', '$GNGGA']
-
+print 'parsing!!! please wait...'
 for line in open(sourceFile, 'r'):
 #	print (line)
 	for s in compareObject:
 		if s in line:
+			index = line.find(s)
+			#print line[index:]
 			with open(targetFile, 'a') as writeFile:
-				writeFile.write(line)
+				writeFile.write(line[index:])
 print 'done, please check NMEASentence.txt at the current directory'
